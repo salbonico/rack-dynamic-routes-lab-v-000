6b164f67498dcefef50924b3b7a9d89517dd0d47
Steve Albonico <salbonico@gmail.com>
2019-02-01 20:46:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,10 +6,11 @@ class Application
   def call(env)
       resp = Rack::Response.new
       req = Rack::Request.new(env)
+      binding.pry
         if req.path.match(/items/)
           item = req.path.split(/items/).last
           @@items.detect{|item| item["name"] == item} != nil
-          
+
 
         else
           resp.write "Route not found"
