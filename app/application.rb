ec606328f1efde35d14f61223239ea42e6166549
Steve Albonico <salbonico@gmail.com>
2019-02-01 20:48:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
 
         if req.path.match(/items/)
           item = req.path.split(/items/).last
-          @@items.detect{|item| item["name"] == item}
+          what = @@items.detect{|item| item["name"] == item}
         else
           resp.write "Route not found"
           resp.status = 404
