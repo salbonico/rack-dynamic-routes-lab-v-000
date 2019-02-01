efaabd9f06c886ec0e4b7f8da9ab05ea6ef2a58d
Steve Albonico <salbonico@gmail.com>
2019-02-01 20:24:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,6 @@ class Application
   def call(env)
       resp = Rack::Response.new
       req = Rack::Request.new(env)
-
-
+      if req.path.match(/items/)
+        item = req.path.split(/items/).last
 end
